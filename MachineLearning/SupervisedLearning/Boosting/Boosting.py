56926100ecb7853875f72ecbd74e9309a6b29117
johnmrncc1701d <48630591+johnmrncc1701d@users.noreply.github.com>
2022-02-12 22:39:48 -0800

    Add files via upload

--- MachineLearning/SupervisedLearning/Boosting/Boosting.py
+++ MachineLearning/SupervisedLearning/Boosting/Boosting.py
@@ -211,7 +211,7 @@ class EL_Boosting:
 
 num = 6000 #332 #6000
 
-if True:
+if False:
     depth = 3
     prune = 0.0016
     percent = 0
@@ -248,9 +248,9 @@ if False:
     result = np.asfarray(hlsList)
     np.savetxt("../Sarcasm/Sarcasm_ELB_result_grid.csv", result, delimiter=",")
 
-if False:
-    depth = 3
-    prune = 0.0016
+if True:
+    depth = 2
+    prune = 0.0009
     random_state = 1
     percentList = [[0, 0, None],[0, 1, None],[0, 2, None],[0, 3, None]]
     for i in range(1,5):
@@ -258,14 +258,93 @@ if False:
         for j in range(1,5):
             if i == j: continue
             fileList.append(f"../Sarcasm/Sarcasm_train_set_{j}.csv")
-            
-        d = EL_Boosting(fileList, num, prune, depth, 2, False, True, 100, random_state)
+        d = EL_Boosting(fileList, num, prune, depth, 2, False, True, 25, random_state)
         percentList[i-1][0] = d.Predict(f"../Sarcasm/Sarcasm_train_set_{i}.csv", num*3, False, f"../Sarcasm/Sarcasm_ELB_result_{i}.csv")
         percentList[i-1][2] = d
 
     percentList.sort(reverse=True)
     percent = percentList[0][2].Predict("../Sarcasm/Sarcasm_test_set.csv", num*3, False, "../Sarcasm/Sarcasm_ELB_final.csv")
     print("Percent Match: " + str(percent))
+
+if False:
+    depth = 3
+    prune = 0.0016
+    random_state = 1
+    testMinNum = 500
+    testMaxNum = 6100
+    testNumStep = 500
+
+    numList = [[d for d in range(testMinNum, testMaxNum, testNumStep)], [0 for d in range(testMinNum, testMaxNum, testNumStep)], [0 for d in range(testMinNum, testMaxNum, testNumStep)]]
+    i = 0
+    for numb in np.arange(testMinNum, testMaxNum, testNumStep):
+        d = EL_Boosting(["../Sarcasm/Sarcasm_train_set_1.csv","../Sarcasm/Sarcasm_train_set_2.csv","../Sarcasm/Sarcasm_train_set_3.csv"], numb, prune, depth, 2, False, True, 100, random_state)
+        print("Num: " + str(numb))
+        start = time.perf_counter()
+        percent = d.Predict("../Sarcasm/Sarcasm_train_set_4.csv", num*3, False, "../Sarcasm/Sarcasm_result_ELB_0.csv")
+        totalTime = time.perf_counter() - start
+        print("Total Time: " + str(totalTime))
+        numList[1][i] = percent
+        numList[2][i] = totalTime
+        i += 1
+    result = np.asfarray(numList)
+    np.savetxt("../Sarcasm/Sarcasm_result_ELB_vsSize.csv", result, delimiter=",")
+
+if False:
+    depth = 3
+    random_state = 1
+    prune_min = 0.0001
+    prune_max = 0.002
+    prune_step = 0.0001
+
+    hlsList = [[d for d in np.arange(prune_min, prune_max, prune_step)], [0 for d in np.arange(prune_min, prune_max, prune_step)]]
+    i = 0
+    for prune in np.arange(prune_min, prune_max, prune_step):
+        d = EL_Boosting(["../Sarcasm/Sarcasm_train_set_1.csv","../Sarcasm/Sarcasm_train_set_2.csv","../Sarcasm/Sarcasm_train_set_3.csv"], num, prune, depth, 2, False, True, 100, random_state)
+        print("Prune: " + str(prune))
+        percent = d.Predict("../Sarcasm/Sarcasm_train_set_4.csv", num*3, False, "../Sarcasm/Sarcasm_result_ELB_0.csv")
+        hlsList[1][i] = percent
+        i += 1
+    result = np.asfarray(hlsList)
+    np.savetxt("../Sarcasm/Sarcasm_result_ELB_prune.csv", result, delimiter=",")
+
+if False:
+    depth_min = 1
+    depth_max = 5
+    depth_step = 1
+    random_state = 1
+    prune = 0.0016
+
+    hlsList = [[d for d in np.arange(depth_min, depth_max, depth_step)], [0 for d in np.arange(depth_min, depth_max, depth_step)]]
+    i = 0
+    for depth in np.arange(depth_min, depth_max, depth_step):
+        d = EL_Boosting(["../Sarcasm/Sarcasm_train_set_1.csv","../Sarcasm/Sarcasm_train_set_2.csv","../Sarcasm/Sarcasm_train_set_3.csv"], num, prune, depth, 2, False, True, 100, random_state)
+        print("Depth: " + str(depth))
+        percent = d.Predict("../Sarcasm/Sarcasm_train_set_4.csv", num*3, False, "../Sarcasm/Sarcasm_result_ELB_0.csv")
+        hlsList[1][i] = percent
+        i += 1
+    result = np.asfarray(hlsList)
+    np.savetxt("../Sarcasm/Sarcasm_result_ELB_depth.csv", result, delimiter=",")
+
+if False:
+    depth = 3
+    random_state = 1
+    prune = 0.0016
+    est_min = 5
+    est_max = 105
+    est_step = 10
+
+    hlsList = [[d for d in np.arange(est_min, est_max, est_step)], [0 for d in np.arange(est_min, est_max, est_step)]]
+    i = 0
+    for est in np.arange(est_min, est_max, est_step):
+        d = EL_Boosting(["../Sarcasm/Sarcasm_train_set_1.csv","../Sarcasm/Sarcasm_train_set_2.csv","../Sarcasm/Sarcasm_train_set_3.csv"], num, prune, depth, 2, False, True, est, random_state)
+        print("Estimators: " + str(est))
+        percent = d.Predict("../Sarcasm/Sarcasm_train_set_4.csv", num*3, False, "../Sarcasm/Sarcasm_result_ELB_0.csv")
+        hlsList[1][i] = percent
+        i += 1
+    result = np.asfarray(hlsList)
+    np.savetxt("../Sarcasm/Sarcasm_result_ELB_est.csv", result, delimiter=",")
+
+num = 2000
     
 # removing low variance speeds it up but doesn't seem to improve or worsen the results much
 # at 10 iterations ~ 72.5%
@@ -299,9 +378,9 @@ if False:
     result = np.asfarray(hlsList)
     np.savetxt("../Sentiment/Sentiment_ELB_result_grid.csv", result, delimiter=",")
 
-if False:
-    depth = 3
-    prune = 0.0008
+if True:
+    depth = 4
+    prune = 0.0013
     random_state = 1
     percentList = [[0, 0, None],[0, 1, None],[0, 2, None],[0, 3, None]]
     for i in range(1,5):
@@ -310,14 +389,94 @@ if False:
             if i == j: continue
             fileList.append(f"../Sentiment/Sentiment_train_set_{j}.csv")
             
-        d = EL_Boosting(fileList, num, prune, depth, 2, False, True, 100, random_state)
+        d = EL_Boosting(fileList, num, prune, depth, 2, False, True, 35, random_state)
         percentList[i-1][0] = d.Predict(f"../Sentiment/Sentiment_train_set_{i}.csv", num*3, False, f"../Sentiment/Sentiment_ELB_result_{i}.csv")
         percentList[i-1][2] = d
 
     percentList.sort(reverse=True)
     percent = percentList[0][2].Predict("../Sentiment/Sentiment_test_set.csv", num*3, False, "../Sentiment/Sentiment_ELB_final.csv")
     print("Percent Match: " + str(percent))
+
+if False:
+    depth = 3
+    prune = 0.0016
+    random_state = 1
+    testMinNum = 500
+    testMaxNum = 6100
+    testNumStep = 500
+
+    numList = [[d for d in range(testMinNum, testMaxNum, testNumStep)], [0 for d in range(testMinNum, testMaxNum, testNumStep)], [0 for d in range(testMinNum, testMaxNum, testNumStep)]]
+    i = 0
+    for numb in np.arange(testMinNum, testMaxNum, testNumStep):
+        d = EL_Boosting(["../Sentiment/Sentiment_train_set_1.csv","../Sentiment/Sentiment_train_set_2.csv","../Sentiment/Sentiment_train_set_3.csv"], numb, prune, depth, 2, False, True, 100, random_state)
+        print("Num: " + str(numb))
+        start = time.perf_counter()
+        percent = d.Predict("../Sentiment/Sentiment_train_set_4.csv", num*3, False, "../Sentiment/Sentiment_result_ELB_0.csv")
+        totalTime = time.perf_counter() - start
+        print("Total Time: " + str(totalTime))
+        numList[1][i] = percent
+        numList[2][i] = totalTime
+        i += 1
+    result = np.asfarray(numList)
+    np.savetxt("../Sentiment/Sentiment_result_ELB_vsSize.csv", result, delimiter=",")
     
+num = 1500
+
+if False:
+    depth = 3
+    random_state = 1
+    prune_min = 0.0001
+    prune_max = 0.002
+    prune_step = 0.0001
+
+    hlsList = [[d for d in np.arange(prune_min, prune_max, prune_step)], [0 for d in np.arange(prune_min, prune_max, prune_step)]]
+    i = 0
+    for prune in np.arange(prune_min, prune_max, prune_step):
+        d = EL_Boosting(["../Sentiment/Sentiment_train_set_1.csv","../Sentiment/Sentiment_train_set_2.csv","../Sentiment/Sentiment_train_set_3.csv"], num, prune, depth, 2, False, True, 100, random_state)
+        print("Prune: " + str(prune))
+        percent = d.Predict("../Sentiment/Sentiment_train_set_4.csv", num*3, False, "../Sentiment/Sentiment_result_ELB_0.csv")
+        hlsList[1][i] = percent
+        i += 1
+    result = np.asfarray(hlsList)
+    np.savetxt("../Sentiment/Sentiment_result_ELB_prune.csv", result, delimiter=",")
+
+if False:
+    depth_min = 1
+    depth_max = 5
+    depth_step = 1
+    random_state = 1
+    prune = 0.0016
+
+    hlsList = [[d for d in np.arange(depth_min, depth_max, depth_step)], [0 for d in np.arange(depth_min, depth_max, depth_step)]]
+    i = 0
+    for depth in np.arange(depth_min, depth_max, depth_step):
+        d = EL_Boosting(["../Sentiment/Sentiment_train_set_1.csv","../Sentiment/Sentiment_train_set_2.csv","../Sentiment/Sentiment_train_set_3.csv"], num, prune, depth, 2, False, True, 100, random_state)
+        print("Depth: " + str(depth))
+        percent = d.Predict("../Sentiment/Sentiment_train_set_4.csv", num*3, False, "../Sentiment/Sentiment_result_ELB_0.csv")
+        hlsList[1][i] = percent
+        i += 1
+    result = np.asfarray(hlsList)
+    np.savetxt("../Sentiment/Sentiment_result_ELB_depth.csv", result, delimiter=",")
+
+if False:
+    depth = 3
+    random_state = 1
+    prune = 0.0016
+    est_min = 5
+    est_max = 105
+    est_step = 10
+
+    hlsList = [[d for d in np.arange(est_min, est_max, est_step)], [0 for d in np.arange(est_min, est_max, est_step)]]
+    i = 0
+    for est in np.arange(est_min, est_max, est_step):
+        d = EL_Boosting(["../Sentiment/Sentiment_train_set_1.csv","../Sentiment/Sentiment_train_set_2.csv","../Sentiment/Sentiment_train_set_3.csv"], num, prune, depth, 2, False, True, est, random_state)
+        print("Estimators: " + str(est))
+        percent = d.Predict("../Sentiment/Sentiment_train_set_4.csv", num*3, False, "../Sentiment/Sentiment_result_ELB_0.csv")
+        hlsList[1][i] = percent
+        i += 1
+    result = np.asfarray(hlsList)
+    np.savetxt("../Sentiment/Sentiment_result_ELB_est.csv", result, delimiter=",")
+
 ################
 
 num = 2500
